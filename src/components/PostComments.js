62acd2dcea95a0af76051ce56a77559aee8c28de
tanishk15sharma <tanishk1582002@gmail.com>
2022-05-16 14:42:30 +0530

    comments component

---
+++ src/components/PostComments.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const PostComments = () => {
+  return (
+    <div>
+      PostComments
+      <h2>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dolore dicta
+        distinctio ea at facilis placeat exercitationem fugiat vero accusamus
+        reiciendis nam sit fuga, explicabo incidunt omnis praesentium enim iure?
+      </h2>
+    </div>
+  );
+};
+
+export { PostComments };
