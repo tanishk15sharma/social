148401aafa869b4540e0f4f72143c4c4082fc074
tanishk15sharma <tanishk1582002@gmail.com>
2022-05-16 16:43:11 +0530

    style : comments section

--- src/components/PostComments.js
+++ src/components/PostComments.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PostComments = () => {
+  const [writeComment, setWriteComment] = useState("");
+  console.log(writeComment);
   return (
     <div>
-      PostComments
-      <h2>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dolore dicta
-        distinctio ea at facilis placeat exercitationem fugiat vero accusamus
-        reiciendis nam sit fuga, explicabo incidunt omnis praesentium enim iure?
-      </h2>
+      <div className="flex  ml-4 ">
+        <div className="w-7 h-7 bg-primary-200 rounded-full flex justify-center mr-4 items-center font-bold text-primary-900">
+          T
+        </div>
+        <input
+          placeholder="write a comment"
+          className="focus:outline-none border-solid border w-full"
+          onChange={(e) => setWriteComment(e.target.value)}
+          value={writeComment}
+        />
+        <button
+          class="text-primary-500 border border-primary-500 hover:bg-primary-500 hover:text-white active:bg-primary-600 font-bold uppercase text-xs px-4 py-2 rounded outline-none focus:outline-none  ease-linear transition-all duration-150"
+          type="button"
+        >
+          Add
+        </button>
+      </div>
+      <div className="flex items-end ml-4 mb-2 mt-2">
+        <div className="w-7 h-7 bg-primary-200 rounded-full flex justify-center mr-4 items-center font-bold text-primary-900">
+          T
+        </div>
+        <p className="text-sm">this is taishk is an awesome</p>
+      </div>
+      {/* <div>
+        <div className="w-9 h-9 bg-primary-200 rounded-full flex justify-center mr-2 items-center font-bold text-primary-900">
+          T Tanishk is a good boy
+        </div>
+      </div> */}
     </div>
   );
 };
