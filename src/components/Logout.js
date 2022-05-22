c0372d2f171b4a8b30a1142539309b726ae3e425
tanishk15sharma <tanishk1582002@gmail.com>
2022-05-22 19:46:50 +0530

    feat:logout

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Logout = () => {
+  const navigate = useNavigate();
+
+  const logoutHandler = () => {
+    localStorage.clear();
+    navigate("/login");
+  };
   return (
     <div>
-      <button className="p-1 bg-primary-50 w-full hover:bg-primary-200">
+      <button
+        className="p-1 bg-primary-50 w-full hover:bg-primary-200"
+        onClick={logoutHandler}
+      >
         Logout
       </button>
     </div>
